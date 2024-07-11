0bbee8fc5c72369c3239496f5fe742d35c05b993
Pascal Gouasmi <pgouasmi@student.42lyon.fr>
2024-07-11 14:59:52 +0200

    rmed prints

--- ball.py
+++ ball.py
@@ -25,6 +25,7 @@ class Ball:
         self.x_vel = self.max_speed / 3
         self.y_vel = 0
         self.frictionTimestamp = time.time()
+        self.x = win_width - 10
 
 
     def draw(self, win):
